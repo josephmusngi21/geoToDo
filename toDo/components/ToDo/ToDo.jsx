a46b326d9edc9c19ed485dbeeb29b78e2dff0f09
Josephmusngi21 <josephmusngi7@gmail.com>
2025-05-19 22:12:32 -0700

    started working on indiviual page for menu options

--- toDo/components/ToDo/ToDo.jsx
+++ toDo/components/ToDo/ToDo.jsx
@@ -1,38 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 
 export default function ToDo() {
-  const Menu = () => {
-    return (
-      <View style={styles.taskContainer}>
-        <TouchableOpacity style={styles.task}>
-          <Text style={styles.taskText}>Locations</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.task}>
-          <Text style={styles.taskText}>Today&apos;s Task</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.task}>
-          <Text style={styles.taskText}>Future Task</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
+    const [currentMenu, setCurrentMenu] = useState("Locations");
+  
+    const Today = () => {
+
+        return (
+            <View style={styles.todayContainer}>
+
+            </View>
+        )
+    };
+
+        const Locations = () => {
+
+        return (
+            <View style={styles.locationsContainer}>
+
+            </View>
+        )
+    };
+
+        const Future = () => {
+
+        return (
+            <View style={styles.FutureContainer}>
+
+            </View>
+        )
+    };
 
   return (
     <View style={styles.container}>
-      <Menu />
+      
+        <View style={styles.taskContainer}>
+            <TouchableOpacity style={styles.task}>
+            <Text style={styles.taskText}>Locations</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.task}>
+            <Text style={styles.taskText}>Today&apos;s Task</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.task}>
+            <Text style={styles.taskText}>Future Task</Text>
+            </TouchableOpacity>
+      </View>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-  },
-  taskContainer: {
-
-  },
-  task: {
-    
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    taskContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        backgroundColor: "lightblue", 
+    },
+    task: {
+        padding: 10,
+        backgroundColor: "#e0e0e0",
+        borderRadius: 5,
+        marginHorizontal: 5,
+    },
+    taskText: {
+        fontSize: 16,
+        color: "#333",
+    },
 });
