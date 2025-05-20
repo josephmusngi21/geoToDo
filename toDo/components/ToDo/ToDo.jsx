0272d489e4444e064578b34e143f976ffa818c2a
Josephmusngi21 <josephmusngi7@gmail.com>
2025-05-19 22:00:45 -0700

    added Menus

--- toDo/components/ToDo/ToDo.jsx
+++ toDo/components/ToDo/ToDo.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 
 export default function ToDo() {
-
+  const Menu = () => {
     return (
-        <View style={styles.container}>
+      <View style={styles.taskContainer}>
+        <TouchableOpacity style={styles.task}>
+          <Text style={styles.taskText}>Locations</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.task}>
+          <Text style={styles.taskText}>Today&apos;s Task</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.task}>
+          <Text style={styles.taskText}>Future Task</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Menu />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({ 
-    container: {
-        display: "flex",
-    },
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+  },
+  taskContainer: {
+
+  },
+  task: {
+    
+  },
 });
