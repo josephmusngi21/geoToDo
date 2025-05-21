f70b48fa58dde81b9f45c895c38e00d52fa07012
Josephmusngi21 <josephmusngi7@gmail.com>
2025-05-21 01:26:42 -0700

    added al components worked on

--- toDo/app/index.tsx
+++ toDo/app/index.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
-// import User from './../components/User/User';
-// import ToDo from './../components/ToDo/ToDo';
+import { StyleSheet, View } from "react-native";
+import User from './../components/User/User';
+import ToDo from './../components/ToDo/ToDo';
 import Task from '../components/ToDo/Tabs/components/Task'
+import TodayTab from "@/components/ToDo/Tabs/TodayTab";
+
 export default function Index() {
   return (
     <View style={styles.container}>
+      <User />
+      <TodayTab />
+      <ToDo />
       <Task task={null} />
     </View>
   );
