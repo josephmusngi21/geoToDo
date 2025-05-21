1904c2dd2a88e4aaa339756de50cc44e26e9096e
Josephmusngi21 <josephmusngi7@gmail.com>
2025-05-21 01:17:11 -0700

    showing Task to work on

--- toDo/app/index.tsx
+++ toDo/app/index.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 // import User from './../components/User/User';
-import ToDo from './../components/ToDo/ToDo';
-
+// import ToDo from './../components/ToDo/ToDo';
+import Task from '../components/ToDo/Tabs/components/Task'
 export default function Index() {
   return (
     <View style={styles.container}>
-      <ToDo />
+      <Task task={null} />
     </View>
   );
 }
