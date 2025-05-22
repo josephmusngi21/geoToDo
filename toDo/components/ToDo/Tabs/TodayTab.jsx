f8b3cb20c4ace338b716f86f7bc9e1f1c2d5ebaa
Josephmusngi21 <josephmusngi7@gmail.com>
2025-05-22 00:25:10 -0700

    imported examples and mapped, need to fix scrolling problem

--- toDo/components/ToDo/Tabs/TodayTab.jsx
+++ toDo/components/ToDo/Tabs/TodayTab.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from "react-native";
+import Task from "./components/Task";
+
+import examples from './components/tasks.json'
+
 
 export default function TodayTab() {
   const today = new Date();
@@ -9,12 +13,20 @@ export default function TodayTab() {
     day: "numeric",
   });
 
+      // Categorize tasks by status
+      const tasksByStatus = {
+        All: examples,
+        Open: examples.filter(task => task.status.toLowerCase() === "open"),
+        Closed: examples.filter(task => task.status.toLowerCase() === "closed"),
+        WIP: examples.filter(task => task.status.toLowerCase() === "wip"),
+      };
+
+      // For the menu counts
       const taskMenus = {
-        //The int for each of these will eventuall be replaced with the number of tasks in each category from mongoDb
-        All: [0],
-        Open: [0],
-        Closed: [0],
-        WIP: [0],
+        All: tasksByStatus.All.length,
+        Open: tasksByStatus.Open.length,
+        Closed: tasksByStatus.Closed.length,
+        WIP: tasksByStatus.WIP.length,
       };
 
   return (
@@ -31,7 +43,7 @@ export default function TodayTab() {
           </TouchableOpacity>
       </View>
 
-        <View style={styles.taskContainer}>
+        <View style={styles.countContainer}>
             {Object.entries(taskMenus).map(([key, value]) => (
                 <TouchableOpacity
                 key={key}
@@ -42,75 +54,95 @@ export default function TodayTab() {
                 </TouchableOpacity>
             ))}
         </View>
+            
+        <View style={styles.taskContainer}>
+            {examples.map((task, index) => (
+                <Task
+                    key={index}
+                    task={task}
+                />
+            ))}
+          
+        </View>
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        display: "flex", 
-        flexDirection: "column",
-        width: "93%",
-        marginHorizontal: 10,
-    },
-    infoContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        paddingVertical: 7,
-        marginVertical: 24,
+  container: { 
+    display: "flex", 
+    flexDirection: "column",
+    width: "93%",
+    marginHorizontal: 10,
+  },
+  infoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingVertical: 7,
+    marginVertical: 24,
 
-    },
-    info: {
-        paddingLeft: 20,
-    },
-    infoText: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "black",
-    },
-    dateText: {
-        fontSize: 13,
-        color: "#878787",
-    },
-    addButton: {
-        width: 180,
-        height: 47,
-        backgroundColor: "#E2EBFA",
-        borderRadius: 20,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    addText: {
-        fontSize: 13,
-        color: "#1756D2",
-        fontWeight: "bold",
-    },
-    taskContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        paddingVertical: 7,
-    },
-    task: {
-        display: "flex",
-        flexDirection: "row",
-    },
-    taskText: {
-        fontSize: 15,
-        fontWeight: 570,
-        paddingHorizontal: 10,
-    },
-    taskCount: {
-        fontSize: 13,
-        fontWeight: 600,
-        color: "#878787",
-        marginLeft: 6,
-    },
+  },
+  info: {
+    paddingLeft: 20,
+  },
+  infoText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+  },
+  dateText: {
+    fontSize: 13,
+    color: "#878787",
+  },
+  addButton: {
+    width: 180,
+    height: 47,
+    backgroundColor: "#E2EBFA",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {
+    fontSize: 13,
+    color: "#1756D2",
+    fontWeight: "bold",
+  },
+  countContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingVertical: 7,
+  },
+  task: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  taskText: {
+    fontSize: 15,
+    fontWeight: 570,
+    paddingHorizontal: 10,
+  },
+  taskCount: {
+    fontSize: 13,
+    fontWeight: 600,
+    color: "#878787",
+    marginLeft: 6,
+  },
+  taskContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingVertical: 7,
+    // Remove overflow: "scroll", as it's not supported in React Native
+    // overflow: "scroll",
+  },
 
 });
